79da96b604b226670b34959bde83eb0a6482d36a
Enzo <ferenaye@gmail.com>
2023-10-19 15:15:42 +0200

    BDD v2

--- sae_belfo.sql
+++ sae_belfo.sql
@@ -1,6 +1,25 @@
+DROP TABLE if exists accueil;
+DROP TABLE if exists possède;
+DROP TABLE if exists vend;
+DROP TABLE if exists reservation;
+DROP TABLE if exists emplacement;
+DROP TABLE if exists attraction;
+DROP TABLE if exists scene;
+DROP TABLE if exists stand;
+DROP TABLE if exists prestataire;
+DROP TABLE if exists client;
+DROP TABLE if exists utilisateur;
+DROP TABLE if exists produit;
+DROP TABLE if exists intervenant;
+DROP TABLE if exists type;
+DROP TABLE if exists crenaux;
+DROP TABLE if exists tags;
+DROP TABLE if exists disponibilite;
+DROP TABLE if exists taggue;
+
 CREATE TABLE utilisateur(
    id_user INT,
-   permission BYTE,
+   permission INT,
    login VARCHAR(50),
    password VARCHAR(50),
    PRIMARY KEY(id_user)
@@ -19,7 +38,7 @@ CREATE TABLE client(
 CREATE TABLE produit(
    id_produit VARCHAR(50),
    nom VARCHAR(50),
-   prix CURRENCY,
+   prix NUMERIC(5,2),
    stock INT,
    PRIMARY KEY(id_produit)
 );
@@ -39,7 +58,7 @@ CREATE TABLE type(
 );
 
 CREATE TABLE crenaux(
-   date_reservation DATETIME,
+   date_reservation timestamp,
    PRIMARY KEY(date_reservation)
 );
 
@@ -60,30 +79,6 @@ CREATE TABLE prestataire(
    FOREIGN KEY(id_user) REFERENCES utilisateur(id_user)
 );
 
-CREATE TABLE stand(
-   id_stand VARCHAR(50),
-   description VARCHAR(50),
-   id_type VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_stand),
-   FOREIGN KEY(id_type) REFERENCES type(id_type)
-);
-
-CREATE TABLE scene(
-   id_scene VARCHAR(50),
-   description VARCHAR(50),
-   id_type VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_scene),
-   FOREIGN KEY(id_type) REFERENCES type(id_type)
-);
-
-CREATE TABLE attraction(
-   id_attraction VARCHAR(50),
-   description VARCHAR(255),
-   id_type VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_attraction),
-   FOREIGN KEY(id_type) REFERENCES type(id_type)
-);
-
 CREATE TABLE emplacement(
    id_emplacement VARCHAR(50),
    libelle VARCHAR(50),
@@ -94,17 +89,40 @@ CREATE TABLE emplacement(
 
 CREATE TABLE reservation(
    id_reservation VARCHAR(50),
-   date_reservation DATETIME NOT NULL,
+   id_emplacement VARCHAR(50) NOT NULL,
+   date_reservation timestamp NOT NULL,
    id_client VARCHAR(50) NOT NULL,
-   id_stand VARCHAR(50),
-   id_scene VARCHAR(50),
-   id_attraction VARCHAR(50),
    PRIMARY KEY(id_reservation),
+   FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement),
    FOREIGN KEY(date_reservation) REFERENCES crenaux(date_reservation),
-   FOREIGN KEY(id_client) REFERENCES client(id_client),
-   FOREIGN KEY(id_stand) REFERENCES stand(id_stand),
-   FOREIGN KEY(id_scene) REFERENCES scene(id_scene),
-   FOREIGN KEY(id_attraction) REFERENCES attraction(id_attraction)
+   FOREIGN KEY(id_client) REFERENCES client(id_client)
+);
+
+CREATE TABLE stand(
+   id_stand VARCHAR(50),
+   description VARCHAR(50),
+   id_emplacement VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_stand),
+   UNIQUE(id_emplacement),
+   FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement)
+);
+
+CREATE TABLE scene(
+   id_scene VARCHAR(50),
+   description VARCHAR(50),
+   id_emplacement VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_scene),
+   UNIQUE(id_emplacement),
+   FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement)
+);
+
+CREATE TABLE attraction(
+   id_attraction VARCHAR(50),
+   description VARCHAR(255),
+   id_emplacement VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_attraction),
+   UNIQUE(id_emplacement),
+   FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement)
 );
 
 CREATE TABLE vend(
@@ -123,35 +141,63 @@ CREATE TABLE possède(
    FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement)
 );
 
-CREATE TABLE Asso_18(
-   id_stand VARCHAR(50),
-   date_reservation DATETIME,
-   PRIMARY KEY(id_stand, date_reservation),
-   FOREIGN KEY(id_stand) REFERENCES stand(id_stand),
-   FOREIGN KEY(date_reservation) REFERENCES crenaux(date_reservation)
-);
-
-CREATE TABLE Asso_19(
-   id_scene VARCHAR(50),
-   date_reservation DATETIME,
-   PRIMARY KEY(id_scene, date_reservation),
-   FOREIGN KEY(id_scene) REFERENCES scene(id_scene),
-   FOREIGN KEY(date_reservation) REFERENCES crenaux(date_reservation)
-);
-
-CREATE TABLE Asso_20(
-   id_attraction VARCHAR(50),
-   date_reservation DATETIME,
-   PRIMARY KEY(id_attraction, date_reservation),
-   FOREIGN KEY(id_attraction) REFERENCES attraction(id_attraction),
-   FOREIGN KEY(date_reservation) REFERENCES crenaux(date_reservation)
-);
-
 CREATE TABLE accueil(
    id_scene VARCHAR(50),
    id_intervenant VARCHAR(50),
-   horaire DATETIME,
+   horaire timestamp,
    PRIMARY KEY(id_scene, id_intervenant),
    FOREIGN KEY(id_scene) REFERENCES scene(id_scene),
    FOREIGN KEY(id_intervenant) REFERENCES intervenant(id_intervenant)
 );
+
+CREATE TABLE disponibilite(
+   id_emplacement VARCHAR(50),
+   date_reservation timestamp,
+   PRIMARY KEY(id_emplacement, date_reservation),
+   FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement),
+   FOREIGN KEY(date_reservation) REFERENCES crenaux(date_reservation)
+);
+
+CREATE TABLE taggue(
+   id_emplacement VARCHAR(50),
+   id_tag VARCHAR(50),
+   PRIMARY KEY(id_emplacement, id_tag),
+   FOREIGN KEY(id_emplacement) REFERENCES emplacement(id_emplacement),
+   FOREIGN KEY(id_tag) REFERENCES tags(id_tag)
+);
+
+INSERT INTO type VALUES('1', 'Stand'),
+                      ('2', 'Scene'),
+                      ('3', 'Attraction'),
+                      ('4', 'Parking'),
+                      ('5', 'Toilettes');
+
+
+INSERT INTO utilisateur VALUES(1, 1, 'admin', 'admin'),
+                              (2, 0, 'user', 'user'),
+                              (3, 0, 'user2', 'user2');
+
+INSERT INTO client VALUES('1', 'DUPONT', 'Jean', '1990-01-01', 2);
+
+INSERT INTO prestataire VALUES ('1', 'prestataire qui prestate','prestatata','3','3');
+
+INSERT INTO emplacement VALUES ('1', 'emplacement 1', '1'),
+                                ('2', 'emplacement 2', '2'),
+                                ('3', 'emplacement 3', '3');
+
+INSERT INTO tags VALUES ('1', 'accessible handicapé'),
+                        ('2', 'wifi'),
+                        ('3', 'point d eau'),
+
+                        /* tags pour gérer l'affluence (s'attribuent automatiquement en fct du nombre de réservation*/
+                        ('4', 'surchargé'),
+                        ('5', 'modéré'),
+                        ('6', 'libre');
+
+
+
+
+
+
+
+
