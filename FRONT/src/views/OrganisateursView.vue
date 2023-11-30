d033e5be322e2e982e2a307630cd364505821048
Calixte REYMOND <creymond@univ-fcomte.fr>
2023-11-30 22:32:21 +0100

    debut map user

--- FRONT/src/views/OrganisateursView.vue
+++ FRONT/src/views/OrganisateursView.vue
@@ -1,10 +1,27 @@
 <template>
-<p>Organisateurs</p>
+
+  <div>
+
+
+    <h1 style="padding-top: 300px">Hello</h1>
+
+
+    <Map3DUser/>
+  </div>
+
+
 </template>
 
 <script>
+import Map3DUser from '../components/Map3DUser.vue';
+
+export default {
+  components: {
+    Map3DUser
+  },
+}
+
+
 </script>
 
-<style>
-
-</style>
+<style></style>
