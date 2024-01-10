d3b3b0692ee1b8df7cd3bd2177d86eae9f66cd1c
dnezzar-iut90 <113182098+dnezzar-iut90@users.noreply.github.com>
2024-01-10 07:17:55 +0100

    Push footer (wip encore) et page orga (responsive à moitié fait)
    Letsgo je push enfin un truc ;-;

--- FRONT/src/views/OrganisateursView.vue
+++ FRONT/src/views/OrganisateursView.vue
@@ -5,6 +5,7 @@
         <div class="blurred-image"></div>
         <div class="sharp-image"></div>
       </div>
+      <div class="gradient-overlay"></div>
       <div class="text">
         <p class="title">Organisateurs</p>
         <p class="subtitle">Découvrez les têtes pensantes du projet Belforaine !</p>
@@ -31,25 +32,70 @@
 
     <hr>
 
+    <p class="bigText" id="teamForaine" ><strong>La</strong> TeamForaine</p>
+
+    <div class="background">
+
+
+    <div class="organisateurs-container">
+  <CardOrganisateur class="row-1"
+      :profileImage="'stephane.jpg'"
+      :name="'M\'hammed Mechroubi'"
+      :message="'Back-end et front-end developer du projet, j\'ai travaillé sur le design et les fonctionnalités, l\'API et la sécurisation du site'"
+      :email="'m_hammed.mechroubi@edu.univ-fcomte.fr'"
+    />
+
+  <CardOrganisateur class="row-1"
+      :profileImage="'stephane.jpg'"
+      :name="'Calixte Reymond'"
+      :message="'Back-end et front-end developer du projet, j\'ai travaillé sur l\'implémentation de la MAP 3D en ThreeJS'"
+      :email="'calixte.reymond@edu.univ-fcomte.fr'"
+    />
+  <CardOrganisateur class="row-2"
+      :profileImage="'stephane.jpg'"
+      :name="'Zekeriya Akburak'"
+      :message="'Back-end et front-end developer du projet, j\'ai travaillé sur l\'implémentation des fonctionnalités, la sécurité et le design'"
+      :email="'zekeriya.akburak@edu.univ-fcomte.fr'"
+    />
+
+    <CardOrganisateur class="row-3"
+      :profileImage="'stephane.jpg'"
+      :name="'Enzo Bellicaud'"
+      :message="'Back-end developer du projet, j\'ai travaillé sur la base de données et l\'API'"
+      :email="'enzo.bellicaud@edu.univ-fcomte.fr'"
+    />
+
+    <CardOrganisateur class="row-3"
+      :profileImage="'stephane.jpg'"
+      :name="'Dalia Nezzar'"
+      :message="'Majoritairement front-end developer et SCRUM master, j\'ai travaillé sur le design et les fonctionnalités du site ainsi que la gestion du projet'"
+      :email="'dalia.nezzar@edu.univ-fcomte.fr'"
+    />
+  </div>
+
+  </div>
+
+  <FooTer/>
 
   </div>
 
 
 </template>
 
+
+
 <script>
+import CardOrganisateur from '@/components/CardOrganisateur.vue';
+
+import FooTer from '@/components/FooTer.vue';
 
 export default {
   name: "OrganisateursView",
-  data: () => ({
-  }),
-  methods: {
+  components: {
+    CardOrganisateur,
+    FooTer
   },
-  mounted() {
-  }
-}
-
-
+};
 </script>
 
 <style scoped>
@@ -57,6 +103,12 @@ export default {
 .OrganisateursPage {
   font-family: 'DM Sans Regular';
 }
+
+img{
+  pointer-events: none;
+}
+
+
 .page-banner {
   position: relative;
   height: 30em;
@@ -102,6 +154,7 @@ export default {
   font-size: 3em;
   margin-bottom: 0.5em;
   font-weight: bold;
+  margin-left: 5em;
 }
 
 .subtitle {
@@ -110,13 +163,24 @@ export default {
   transform: translateX(40%);
 }
 
-
+.gradient-overlay {
+  position: absolute;
+  bottom: 0;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, #553c656e, transparent);
+  z-index: 4;
+}
 .storytime {
   margin: 5em 10em;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 2em;
+  overflow: hidden;
 }
+
 
 .p-1 {
   grid-column: 1 / 2;
@@ -125,14 +189,6 @@ export default {
   margin-right: 10%;
 }
 
-.p-1::first-letter {
-  font-size: 2em;
-  font-weight: bold;
-  font-style: italic;
-  float: left;
-  margin-right: 3%;
-  line-height: 1;
-}
 
 .p-2 {
   grid-column: 2 / 2;
@@ -140,23 +196,17 @@ export default {
   text-align: justify;
 }
 
-.p-2::first-letter {
-  font-size: 2em;
-  font-weight: bold;
-  font-style: italic;
-  float: left;
-  margin-right: 3%;
-  line-height: 1;
-}
-
 .p-3 {
   margin: 0 0 0 15em;
   margin-right: 15em;
+  margin-left: 15em;
   grid-column: 1 / 3;
   font-size: 1.2em;
   text-align: justify;
 }
 
+.p-1::first-letter,
+.p-2::first-letter,
 .p-3::first-letter {
   font-size: 2em;
   font-weight: bold;
@@ -166,14 +216,57 @@ export default {
   line-height: 1;
 }
 
-.gradient-overlay {
-  position: absolute;
-  bottom: 0;
-  left: 0;
+
+hr {
+  border: 0;
+  height: 2px;
+  background-image: linear-gradient(to right, rgba(0, 0, 0, 0), #553c65, rgba(0, 0, 0, 0));
+  margin: 5em 10em;
+}
+
+@font-face {
+    font-family: 'Syne Extra Bold', sans-serif;
+    src: url(../assets/fonts/Syne/static/Syne-ExtraBold.ttf);
+    font-weight: 800;
+    font-style: normal;
+  }
+
+#teamForaine.bigText {
+  font-family: 'Syne', sans-serif;
+  font-size: 550%;
+  font-weight: 800;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  color: #553C65;
+}
+
+.background {
+  background-image: repeating-linear-gradient(45deg, rgba(255, 255, 255, 0.38) 0, rgba(102, 62, 144, 0.38) 3px, transparent 0, transparent 100%);
+  background-size: 71px 71px;
+  background-color: rgba(255, 255, 255, 0);
+}
+
+.organisateurs-container {
+  display: flex;
+  flex-wrap: wrap; 
+  justify-content: space-between;
+  margin: 0 5em 0;
+}
+
+
+.row-1 {
+  width: calc(48% - 10px);
+}
+
+.row-2 {
   width: 100%;
-  height: calc(100% - 10em); /* ajustez la valeur en fonction de votre mise en page */
-  background: linear-gradient(to bottom, #800080, transparent);
-  z-index: 4;
+  margin-top: -20%;
+}
+
+.row-3 {
+  width: calc(48% - 10px);
+  margin-top: -10%;
 }
 
 
@@ -185,6 +278,23 @@ export default {
   .subtitle {
     font-size: 1.5em;
   }
+
+  .storytime {
+    margin: 5em 5em;
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .p-1,
+  .p-2,
+  .p-3 {
+    grid-column: 1 / 3;
+    margin-right: 0;
+    margin-left: 0;
+    font-size: 120%;
+    text-align: justify;
+    align-items: center;
+  }
 }
 
 @media screen and (max-width: 992px) {
@@ -194,6 +304,22 @@ export default {
 
   .subtitle {
     font-size: 1.2em;
+  }
+
+  .storytime {
+    margin: 5em 5em;
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .p-1,
+  .p-2,
+  .p-3 {
+    grid-column: 1 / 3;
+    margin-right: 0;
+    font-size: 120%;
+    text-align: justify;
+    align-items: center;
   }
 }
 
@@ -216,6 +342,27 @@ export default {
   .subtitle {
     font-size: 1.2em;
   }
+
+  .storytime {
+    margin: 5em 5em;
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .p-1,
+  .p-2,
+  .p-3 {
+    grid-column: 1 / 3;
+    margin-right: 0;
+    margin-left: 0;
+    font-size: 120%;
+    text-align: justify;
+    align-items: center;
+  }
+
+  #teamForaine.bigText {
+    font-size: 400%;
+  }
 }
 
 @media screen and (max-width: 480px) {
@@ -226,5 +373,38 @@ export default {
   .subtitle {
     font-size: 1.2em;
   }
+
+  .gradient-overlay {
+    width: 110%;
+  }
+
+  .image-container {
+  position: relative;
+  width: 110%;
+  height: 100%;
 }
+
+
+  .p-1,
+  .p-2 {
+    margin-left: 10%;
+    margin-right: 0;
+    font-size: 120%;
+    text-align: justify;
+  }
+
+  .p-3 {
+    margin-left: 10%;
+    margin-right: 0;
+    grid-column: 1 / 3;
+    font-size: 1.2em;
+    text-align: justify;
+  }
+
+  #teamForaine.bigText {
+    font-size: 300%;
+  }
+}
+
+
 </style>
