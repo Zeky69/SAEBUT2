7c65c38b97427cffe3afaac935255b0e1137eba2
Enzo <ferenaye@gmail.com>
2024-01-11 20:31:43 +0100

    group by dans reservation

--- BACKEND/src/services/reservations.service.js
+++ BACKEND/src/services/reservations.service.js
@@ -17,12 +17,13 @@ async function test (req,callback) {
     }
 }
 
-async function getAllResa (req,callback) {
-    let id_emp = req.query.id_emp;
+async function getAllResaByIdUser (req,callback) {
+    let id_bat = req.query.id_bat;
+    let id_client = req.query.id_user;
     const client = await pool.connect();
     try{
-        const query = `SELECT * from reservation where id_client is not null and id_emplacement=$2 ORDER BY ouverture ASC`;
-        res = await client.query(query,[id_client,id_emp]);
+        const query = `SELECT * from reservation where id_client=$1 and id_batiment=$2 ORDER BY ouverture ASC`;
+        res = await client.query(query,[id_client,id_bat]);
         for (let i = 0; i < res.rows.length; i++) {
             res.rows[i].ouverture = res.rows[i].ouverture.toLocaleString('fr-FR', { timeZone: 'Europe/Paris' , year: 'numeric', month: 'numeric', day: 'numeric', hour : "2-digit", minute:"2-digit" });
         }
@@ -35,12 +36,11 @@ async function getAllResa (req,callback) {
 }
 
 async function getAllResaById (req,callback) {
-    let id_client = req.query.user_id;
-    let id_emp = req.query.id_emp;
+    let id_bat = req.query.id_bat;
     const client = await pool.connect();
     try{
-        const query = `SELECT * from reservation where id_client=$1 AND id_emplacement=$2 ORDER BY ouverture ASC`;
-        res = await client.query(query,[id_client,id_emp]);
+        const query = `select ouverture,duree, id_batiment, id_client, count(id_reservation) as amount,(SELECT id_reservation  from reservation r2 where id_batiment=$1 AND id_client IS NOT NULL AND r1.ouverture=r2.ouverture AND r1.duree=r2.duree LIMIT 1)  from reservation r1 where id_batiment=$1 AND id_client IS NOT NULL GROUP BY ouverture,duree, id_batiment, id_client ORDER BY ouverture ASC`;
+        res = await client.query(query,[id_bat]);
         for (let i = 0; i < res.rows.length; i++) {
             res.rows[i].ouverture = res.rows[i].ouverture.toLocaleString('fr-FR', { timeZone: 'Europe/Paris' , year: 'numeric', month: 'numeric', day: 'numeric', hour : "2-digit", minute:"2-digit" });
         }
@@ -53,11 +53,11 @@ async function getAllResaById (req,callback) {
 }
 
 async function getAllDispoById (req,callback) {
-    let id = req.query.id;
+    let id_bat = req.query.id_bat;
     const client = await pool.connect();
     try{
-        const query = `select * from reservation where id_emplacement=$1 AND id_client IS NULL ORDER BY ouverture ASC`;
-        res = await client.query(query,[id]);
+        const query = `select ouverture,duree, id_batiment, id_client, count(id_reservation) as amount,(SELECT id_reservation  from reservation r2 where id_batiment=$1 AND id_client IS NULL AND r1.ouverture=r2.ouverture AND r1.duree=r2.duree LIMIT 1)  from reservation r1 where id_batiment=$1 AND id_client IS NULL GROUP BY ouverture,duree, id_batiment, id_client ORDER BY ouverture ASC`;
+        res = await client.query(query,[id_bat]);
         for (let i = 0; i < res.rows.length; i++) {
             res.rows[i].ouverture = res.rows[i].ouverture.toLocaleString('fr-FR', { timeZone: 'Europe/Paris' , year: 'numeric', month: 'numeric', day: 'numeric', hour : "2-digit", minute:"2-digit" });
         }
@@ -69,32 +69,16 @@ async function getAllDispoById (req,callback) {
     }
 }
 
-async function getAuth (req,callback) {
-    let id_emp = req.query.id_emp;
-    let user_id = req.query.user_id;
-    const client = await pool.connect();
-    try{
-        const query = `select * from possède where id_emplacement=$1 and id_prestataire=$2`;
-        res = await client.query(query,[id_emp,user_id]);
-        callback(null,res.rows);
-    }catch(err){
-        callback(err,null);
-    }finally{
-        client.release();
-    }
-}
-
 async function createDispo (req,callback) {
-    let id_emp = req.body.id_emp;
+    let id_bat = req.body.id_bat;
     let date = req.body.date;
     let duree = req.body.duree;
     let ArrayDuree = duree.split(":");
     duree = parseInt(ArrayDuree[0])*60+parseInt(ArrayDuree[1]);
-    console.log("Test"+req.body.id_emp+req.body.date);
     const client = await pool.connect();
     try{
-        const query = `INSERT INTO reservation (id_emplacement,ouverture,duree) VALUES ($1, $2, $3)`;
-        res = await client.query(query, [id_emp, date, duree]);
+        const query = `INSERT INTO reservation (id_batiment,ouverture,duree) VALUES ($1, $2, $3)`;
+        res = await client.query(query, [id_bat, date, duree]);
         callback(null,"c good");
     }catch(err){
         console.log(err);
@@ -109,8 +93,11 @@ async function reserver(req, callback){
     let id_reservation = req.body.id_dispo;
     const client = await pool.connect();
     try{
-        const query = `update reservation set id_client=$1 where id_reservation=$2`;
+        let query = `select * from reservation where id_reservation=$1`;
+         res = await client.query(query, [id_reservation]);
+        query = `update reservation set id_client=$1 where id_reservation=$2`;
         res = await client.query(query, [id_client, id_reservation]);
+
         callback(null,"c good");
     }catch(err){
         callback(err,null);
@@ -121,7 +108,6 @@ async function reserver(req, callback){
 
 async function deleteDispoById(req, callback){
     let id_reservation = req.body.id_dispo;
-    console.log(id_reservation);
     const client = await pool.connect();
     try{
         const query = `delete from reservation where id_reservation=$1`;
@@ -136,7 +122,6 @@ async function deleteDispoById(req, callback){
 
 async function deleteResaById(req, callback){
     let id_reservation = req.body.id_resa;
-    console.log(id_reservation);
     const client = await pool.connect();
     try{
         const query = `UPDATE reservation SET id_client=NULL WHERE id_reservation=$1`;
@@ -149,11 +134,12 @@ async function deleteResaById(req, callback){
     }
 }
 
+
+
 module.exports = {
     getAllResaById:getAllResaById,
-    getAllResa:getAllResa,
+    getAllResaByIdUser:getAllResaByIdUser,
     getAllDispoById:getAllDispoById,
-    getAuth:getAuth,
     createDispo:createDispo,
     reserver:reserver,
     deleteDispoById:deleteDispoById,
--- FRONT/src/services/batiment.service.js
+++ FRONT/src/services/batiment.service.js
@@ -1,10 +1,39 @@
-import {getRequest} from "@/services/axios.service";
+import {getRequest, postRequest} from "@/services/axios.service";
 
+async function updateDisplayToAPI(data) {
+    return postRequest('/batiment/updateDisplay', data, 'UPDATEDISPLAY')
 
-async function getBatByIdPrestataire(id) {
+}
+async function getBatByIdPrestataireToAPI(id) {
+    console.log(id)
     return await getRequest('/batiment/' + id, 'getBatByIdPrestataire')
 }
 
-export {
-    getBatByIdPrestataire
+async function updateDisplay(data) {
+    try {
+        let answer = await updateDisplayToAPI(data)
+        console.log("Réponse du back",answer)
+        return answer
+    } catch (error) {
+        // Gérer l'erreur ici
+        console.error('Erreur lors de la mise à jour de la disponibilité :', error.message);
+        throw error; // Vous pouvez choisir de gérer l'erreur ici ou la remonter à l'appelant
+    }
+}
+
+export async function getBatByIdPrestataire(id) {
+    try {
+        let answer = await getBatByIdPrestataireToAPI(id)
+        console.log("Réponse du back",answer)
+        return answer
+    } catch (error) {
+        // Gérer l'erreur ici
+        console.error('Erreur lors de la récupération des batiments :', error.message);
+        throw error; // Vous pouvez choisir de gérer l'erreur ici ou la remonter à l'appelant
+    }
+}
+
+export default {
+    getBatByIdPrestataire,
+    updateDisplay
 }
