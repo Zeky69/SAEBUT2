bfc7a8d0b780ff32e8a95205fa13d5a169eff745
Zeky69 <zek.akburak@gmail.com>
2024-04-02 22:27:10 +0200

    oups

--- FRONT/src/services/axios.service.js
+++ FRONT/src/services/axios.service.js
@@ -21,8 +21,8 @@ Ces trois cas sont traités par une unique fonction handleError().
 
 */
 
-// const baseURL = 'https://api.codeky.fr'  // URL de base de l'API
-const baseURL = 'http://localhost:3000'  // URL de base de l'API
+const baseURL = 'https://api.codeky.fr'  // URL de base de l'API
+// const baseURL = 'http://localhost:3000'  // URL de base de l'API
 
 // creation d'un agent axios, avec une config. pour atteindre l'API
 const axiosAgent = axios.create({
