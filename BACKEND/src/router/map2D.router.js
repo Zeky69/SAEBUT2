ada9b038cd308193297d3b3af21e86083ef14bec
bellicaud enzo <enzo.bellicaud@edu.univ-fcomte.fr>
2024-04-10 20:11:12 +0200

    swagger map2D <3 zeky

--- BACKEND/src/router/map2D.router.js
+++ BACKEND/src/router/map2D.router.js
@@ -5,92 +5,222 @@ const {checkEmpIsFree ,isLocationBelongsToProvider, haveAskEmp} = require("../mi
 
 var router = express.Router();
 
+router.get('/emp',map2Dcontroller.getAllEmp);
 /**
- * Récupérer tous les emplacements
- * filtre possible :
- * query :
- * withPrestataire : boolean -> si true, retourne les emplacements avec prestataire accepté
+ * @swagger
+ * /emp:
+ *     get:
+ *       summary: Récupérer tous les emplacements
+ *       parameters:
+ *         - name: withPrestataire
+ *           in: query
+ *           description: Si true, retourne les emplacements avec prestataire accepté
+ *           schema:
+ *             type: boolean
+ *       responses:
+ *         '200':
+ *           description: Liste des emplacements récupérée avec succès
  *
  */
-router.get('/emp',map2Dcontroller.getAllEmp);
 
-/**
- * Créer un emplacement
- * body :
- * {
- * nom : string,
- * description : string,
- * type : string,
- * prix : number,
- * id_prestataire : number
- * }
- */
 router.post('/emp',verifyToken,verifyisAdmin, map2Dcontroller.createEmp);
+/**
+ * @swagger
+ * /emp:
+ *   post:
+ *       summary: Créer un emplacement
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 nom:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *                 type:
+ *                   type: string
+ *                 prix:
+ *                   type: number
+ *                 id_prestataire:
+ *                   type: number
+ *       responses:
+ *         '200':
+ *           description: Emplacement créé avec succès
+ */
 
 /**
  * Supprimer un emplacement
  * params : id de l'emplacement
  */
 router.delete('/emp/:id',verifyToken,verifyisAdmin, map2Dcontroller.deleteEmp);
-
 /**
- * Modifier les informations d'un emplacement
-* params : id de l'emplacement
-* body :
-* {
-* nom : string,
-* description : string,
-*
-* */
+ * @swagger
+ * /emp/{id}:
+ *     delete:
+ *       summary: Supprimer un emplacement
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement à supprimer
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         '200':
+ *           description: Emplacement supprimé avec succès
+ */
+
 router.patch('/emp/:id',verifyToken,verifyisPrestataire,getPrestaireId,isLocationBelongsToProvider, map2Dcontroller.updateEmpPresataire);
-
-
 /**
- * Modifier les informations d'un emplacement par l'admin
- * params : id de l'emplacement
- * body :
- * { description: String
- * ,nom : String
- * ,type_id : number
- * prestataire_id : number
- * matricepoints : Json ,
- * accept : boolean
- * }
+ * @swagger
+ * /emp/{id}:
+ *    patch:
+ *       summary: Modifier les informations d'un emplacement
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement à modifier
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 nom:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *       responses:
+ *         '200':
+ *           description: Informations de l'emplacement modifiées avec succès
  */
 
 router.patch('/emp/admin/:id',verifyToken,verifyisAdmin, map2Dcontroller.updateEmp);
-
 /**
-*  Demander un emplacement
-* params : id de l'emplacement
-*
-* */
+ * @swagger
+ * /emp/admin/{id}:
+ *     patch:
+ *       summary: Modifier les informations d'un emplacement par l'admin
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement à modifier
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 description:
+ *                   type: string
+ *                 nom:
+ *                   type: string
+ *                 type_id:
+ *                   type: integer
+ *                 prestataire_id:
+ *                   type: integer
+ *                 matricepoints:
+ *                   type: object
+ *                 accept:
+ *                   type: boolean
+ *       responses:
+ *         '200':
+ *           description: Informations de l'emplacement modifiées par l'admin avec succès
+ */
+
 router.post('/emp/ask/:id',verifyToken,checkEmpIsFree,getPrestaireId, map2Dcontroller.askEmp);
-
 /**
- * accepter une demande d'emplacement
-* params : id de l'emplacement
-*
-* */
+ * @swagger
+ * /emp/ask/{id}:
+ *     post:
+ *       summary: Demander un emplacement
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement demandé
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         '200':
+ *           description: Demande d'emplacement effectuée avec succès
+ */
+
 router.post('/emp/accept/:id',verifyToken,verifyisAdmin, map2Dcontroller.acceptEmp);
-
 /**
- * refuser une demande d'emplacement
-* params : id de l'emplacement
+ * @swagger
+ * /emp/accept/{id}:
+ *     post:
+ *       summary: Accepter une demande d'emplacement
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement à accepter
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         '200':
+ *           description: Demande d'emplacement acceptée avec succès
  */
+
 router.post('/emp/refuse/:id',verifyToken,verifyisAdmin, map2Dcontroller.refuseEmp);
-
-
 /**
- * libérer un emplacement
- * params : id de l'emplacement
+ * @swagger
+ * /emp/refuse/{id}:
+ *     post:
+ *       summary: Refuser une demande d'emplacement
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement à refuser
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         '200':
+ *           description: Demande d'emplacement refusée avec succès
  */
+
 router.post('/emp/free/:id',verifyToken,verifyisPrestataire,getPrestaireId, haveAskEmp ,map2Dcontroller.freeEmp);
-
 /**
- *   get type prestations
+ * @swagger
+ * /emp/free/{id}:
+ *     post:
+ *       summary: Libérer un emplacement
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID de l'emplacement à libérer
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       responses:
+ *         '200':
+ *           description: Emplacement libéré avec succès
  */
+
 router.get('/type', map2Dcontroller.getType);
+/**
+ * @swagger
+ * /type:
+ *     get:
+ *       summary: Obtenir les types de prestations
+ *       responses:
+ *         '200':
+ *           description: Types de prestations récupérés avec succès
+ */
 
 
 module.exports = router;
